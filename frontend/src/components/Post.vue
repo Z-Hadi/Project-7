30c29dc9250fc95028c1fcde9c431121adf79a07
Z-Hadi <zaid.zan1124.com>
2022-06-27 14:27:53 +0100

    new

--- frontend/src/components/Post.vue
+++ frontend/src/components/Post.vue
@@ -1,8 +1,8 @@
 <template>
 <div class="card col-6 offset-3 col-sm-4 offset-sm-4 mb-5">
-    <header class="post-header">
-        <div >
-          <p class="post-author fw-bold">post author</p>
+    <header class="post-header ps-3">
+        <div class=" d-flex  justify-content-start">
+          <p class="post-author fw-bold col-md-4">post author         </p>
           <time class="post-author">26/06/2022</time>
         </div>
       </header>
@@ -16,34 +16,14 @@
  
   <li class="nav-item  d-flex justify-content-between align-items-center">
     <a class="nav-link "><font-awesome-icon icon="fa-solid fa-eye" />  Read</a>
-      <span class="badge bg-primary rounded-pill">14</span>
+      <span class="badge bg-primary rounded-pill">14 </span>
   </li>
 
 </ul>
 </div>
 
 
-<div class="card col-6 offset-3 col-sm-4 offset-sm-4">
-    <header class="post-header">
-        <div >
-          <p class="post-author fw-bold">post author</p>
-          <time class="post-author">26/06/2022</time>
-        </div>
-      </header>
-  <div class="card-body">
-    <h4 class="card-title">Post With Paraghraph</h4>
-        </div>
-  <p>A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs.  A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs.  A paragraph is a series of sentences that are organized and coherent, and are all related to a single topic. Almost every piece of writing you do that is longer than a few sentences should be organized into paragraphs.</p>
 
-<ul class="nav justify-content-center shadow-none p-3  bg-light rounded">
-
-   <li class="nav-item  d-flex justify-content-between align-items-center">
-    <a class="nav-link "><font-awesome-icon icon="fa-solid fa-eye" />  Read</a>
-      <span class="badge bg-primary rounded-pill">5</span>
-  </li>
-
-</ul>
-</div>
 
 </template>
 
